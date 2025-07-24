ad8376a8ee88051ff77641416ac4a1af00670c22
SAI VARUN <SAI VARUN@DESKTOP-N8FLDK2>
2025-07-24 17:35:35 +0530

    Commit

---
+++ Training/src/Day5/PropertyCalc.java
@@ -0,0 +1,12 @@
+package Day5;
+public class PropertyCalc {
+	public static void main(String[] args) {
+		RealEstate p = new Varadi();
+		p.home();
+		p.apartment();
+		RealEstate p1 = new Varun1();
+		p1.home();
+		p1.apartment();
+		
+	}
+}
