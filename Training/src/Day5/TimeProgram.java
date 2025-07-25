528defe26b479a9b56d00e2c91b96a5fe6f41df0
SAI VARUN <SAI VARUN@DESKTOP-N8FLDK2>
2025-07-25 10:16:09 +0530

    commit

--- Training/src/Day5/TimeProgram.java
+++ Training/src/Day5/TimeProgram.java
@@ -1,25 +1,36 @@
 package Day5;
-import java.util.ArrayList;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
 abstract class TaskReminder {
     abstract void remindTasks();
 }
+
 class DailyTaskReminder extends TaskReminder {
     public void remindTasks() {
         ArrayList<String> tasks = new ArrayList<String>() {{
-           add("Wake up");
-           add("Breakfast");
+            add("Wake up");
+            add("Breakfast");
             add("Join the meet");
             add("Involve in the meet");
             add("Practice Codes");
         }};
 
-        double[] times = {8.00,8.30,8.50, 9.00, 12.00};
+        LocalTime[] times = {
+            LocalTime.of(8, 0),
+            LocalTime.of(8, 30),
+            LocalTime.of(8, 50),
+            LocalTime.of(9, 0),
+            LocalTime.of(12, 0)
+        };
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
 
         System.out.println(" Daily Task Reminder \n");
 
         for (int i = 0; i < tasks.size(); i++) {
-            String pmAm = (times[i] >= 1 && times[i] <= 9) || times[i] > 12 ? "AM" : "PM";
-            System.out.printf("Time: %.2f %s\n", times[i], pmAm);
+            System.out.println("Time: " + times[i].format(formatter));
             System.out.println("Task: " + tasks.get(i));
         }
     }
