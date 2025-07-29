9edc761a966348859b2720d9ad83d48094fdc2aa
SAI VARUN <SAI VARUN@DESKTOP-N8FLDK2>
2025-07-29 16:55:05 +0530

    Commit

---
+++ Training/src/Day9/Wrapper3.java
@@ -0,0 +1,11 @@
+package Day9;
+
+public class Wrapper3 {
+	public static void main(String[]args) {
+		float a = 12.5f;
+		Float F = a;
+		float f  =F;
+		System.out.println(F);
+		System.out.println(f);
+	}
+}
